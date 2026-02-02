e22f61a18c3b85ecb9f65c791ffd5908f28a95ca
ton7788 <tanawut.suk@gmail.com>
2026-02-02 08:52:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,21 @@
-const CACHE_NAME = 'cash-receipt-v1.2.3';
-const assets = ['./', './index.html', './manifest.json', './icon.png', 'https://fonts.googleapis.com/css2?family=Sarabun:wght@300;400;500;600;700&display=swap', 'https://html2canvas.hertzen.com/dist/html2canvas.min.js'];
+const CACHE_NAME = 'cash-receipt-v1.2.4';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  'https://fonts.googleapis.com/css2?family=Sarabun:wght@300;400;500;600;700&display=swap',
+  'https://html2canvas.hertzen.com/dist/html2canvas.min.js'
+];
 
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets))));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
